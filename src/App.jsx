83db920d3b262750a0ac43d014b38bbae4df545a
jum8 <jumeight@gmail.com>
2022-12-02 21:42:38 -0300

    Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
+import { Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import Home from "./Routes/Home";
 
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+						<Routes>
+							<Route path="/home" element={<Home/>} />
+							<Route path="/dentist/:id" element={<Detail/>} />
+							<Route path="/contacto" element={<Contact/>} />
+							<Route path="/favs" element={<Favs/>} />
+						</Routes>
           <Footer/>
       </div>
   );
